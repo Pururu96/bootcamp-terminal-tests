b77669039c257c63d1d67dfddffcc45695e5c742
Pururu <pururumphahlele@gmail.com>
2021-09-07 21:45:09 +0200

    Trial.

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -1,18 +1,17 @@
 let assert = require("assert");
-let fromStellies = require("../countAllFromTown");
-let fromKuilsriver = require("../countAllFromTown");
-let fromMpumalanga = require("../countAllFromTown");
+let countAllFromTown = require("../countAllFromTown");
+
 
 describe('The countAllFromTown function...', function () {
-    it('should count the exact number of vehicle registration numbers from ANY town in a string', function () {
-        assert.equal(fromStellies('CL 124,CY 567,CL 345, CJ 456,CL 341', 'CL'), 3)
+    it('should count the exact number of vehicle registration numbers from Stellies specified town in a string', function () {
+        assert.equal(countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341', 'CL'), 3)
     });
 
-    it('should count the exact number of vehicle registration numbers from ANY town in a string', function () {
-        assert.equal(fromKuilsriver('CJ 124,CY 567,CL 345, CF 456, CL 341', 'CF'), 1)
+    it('should count the exact number of vehicle registration numbers from Kuilsriver in a string', function () {
+        assert.equal(countAllFromTown('CJ 124,CY 567,CL 345, CF 456, CL 341', 'CF'), 1)
     });
 
-    it('should count the exact number of vehicle registration numbers from ANY town in a string', function () {
-        assert.equal(fromMpumalanga('CJ 124,CY 567,CL 345, CF 456, CL 341', 'MP'), 0)
+    it('should count the exact number of vehicle registration numbers from Mpumalanga in a string', function () {
+        assert.equal(countAllFromTown('CJ 124,CY 567,CL 345, CF 456, CL 341', 'MP'), 0)
     });
 });
