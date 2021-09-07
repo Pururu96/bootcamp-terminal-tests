8a6bf10fdd3993d21da96ca9f43042907f0712c2
Pururu <pururumphahlele@gmail.com>
2021-09-07 21:56:02 +0200

    Trial.

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -3,15 +3,15 @@ let countAllFromTown = require("../countAllFromTown");
 
 
 describe('The countAllFromTown function...', function () {
-    it('should count the exact number of vehicle registration numbers from Stellies specified town in a string', function () {
-        assert.equal(countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341', 'CL'), 3)
+    it('should count the exact number of vehicle registration numbers from Stellies in a string', function () {
+        assert.equal(countAllFromTown('CL 124,CY 567,CL 345,CJ 456,CL 341', 'CL'), 3)
     });
 
     it('should count the exact number of vehicle registration numbers from Kuilsriver in a string', function () {
-        assert.equal(countAllFromTown('CJ 124,CY 567,CL 345, CF 456, CL 341', 'CF'), 1)
+        assert.equal(countAllFromTown('CJ 124,CY 567,CL 345,CF 456,CL 341', 'CF'), 1)
     });
 
     it('should count the exact number of vehicle registration numbers from Mpumalanga in a string', function () {
-        assert.equal(countAllFromTown('CJ 124,CY 567,CL 345, CF 456, CL 341', 'MP'), 0)
+        assert.equal(countAllFromTown('45 GY 67,DRT FYL B,DOH 355 NC ,CF 456,CLD 341 L', 'MP'), 0)
     });
 });
