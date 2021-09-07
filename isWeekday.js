cb5a7a9232603baf52ca034110935b2acf12c181
Pururu <pururumphahlele@gmail.com>
2021-09-07 13:54:39 +0200

    Add the isWeekday files for tests.

---
+++ isWeekday.js
@@ -0,0 +1,5 @@
+var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+module.exports = function(weekDays){
+  return weekDays.startsWith('Mon','Tues', 'Wed', 'Thurs', 'Fri');
+};
---
+++ test/isWeekday.test.js
@@ -0,0 +1,11 @@
+let assert = require("assert");
+let isWeekday = require("../isWeekday");
+
+describe('The isWeekday function...', function () {
+    it('should return true if the arguement is a weekday', function () {
+        assert.equal(isWeekday('Monday'), true);
+    });
+    it('should return false if the arguement is NOT a weekday', function () {
+        assert.equal(isWeekday('Saturday'), false);
+    });
+});
