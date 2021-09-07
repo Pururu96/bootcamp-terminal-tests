0b1f1f0eb8f666fdbd5b40a73387345550e319bb
Pururu <pururumphahlele@gmail.com>
2021-09-07 15:28:42 +0200

    Export primary file to node.

--- findItemsOver.js
+++ findItemsOver.js
@@ -1,4 +1,4 @@
-function findItemsOver(object,threshold){ 
+module.exports = function (object,threshold){ 
     var itemsOver20 = [];
    for(var prop in object){
      if(object[prop].qty>threshold){
