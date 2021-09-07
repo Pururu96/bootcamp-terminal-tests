88032892ba18d40c4631439be7350d68f11c2cbc
Pururu <pururumphahlele@gmail.com>
2021-09-07 22:33:37 +0200

    Trial.

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -4,11 +4,11 @@ let countAllFromTown = require("../countAllFromTown");
 
 describe('The countAllFromTown function...', function () {
     it('should count the exact number of vehicle registration numbers from Stellies in a string', function () {
-        assert.equal(countAllFromTown('CL 124,CY 567,CL 345,CJ 456,CL 341', 'CL'), 3)
+        assert.equal(countAllFromTown('CL 124,CY 567,CL 345,CJ 456,CL 341', 'CL'), 1)
     });
 
     it('should count the exact number of vehicle registration numbers from Kuilsriver in a string', function () {
-        assert.equal(countAllFromTown('CJ 124,CY 567,CL 345,CF 456,CL 341', 'CF'), 1)
+        assert.equal(countAllFromTown('CJ 124,CY 567,CL 345,CF 456,CL 341', 'CF'), 0)
     });
 
     it('should count the exact number of vehicle registration numbers from Mpumalanga in a string', function () {
