0eb2e36fbacdad0bb95b3ed18f08c136cd717daa
Pururu <pururumphahlele@gmail.com>
2021-09-07 23:05:19 +0200

    Add scenarios to test files.

--- test/countRegNum.test.js
+++ test/countRegNum.test.js
@@ -2,10 +2,16 @@ let assert = require("assert");
 let countRegNumber = require("../countRegNum");
 
 describe('The countRegNumber function...' , function(){
-    it('should count the number of vehicle registration numbers in a string' , function(){
+    it('should count 3 & 1 if the number of vehicle registration numbers in two separate string strings are 3 & 1 repsectfully' , function(){
         var regCount = countRegNumber('CA 182736,CY 523519,CJ 812328')
         var regCount = countRegNumber('CA 182736')
 
         assert.deepEqual[(regCount, 3), (regCount, 1)]
     });
+
+    it('should count 5 if the number of vehicle registration numbers in a string are 5' , function(){
+        var regCount = countRegNumber('CL 124,CY 567,CL 345,CJ 456,CL 341')
+        assert.equal(regCount,5)
+    });
+
 });
--- test/yearsAgo.test.js
+++ test/yearsAgo.test.js
@@ -1,9 +1,12 @@
 let assert = require("assert");
 let yearsAgo = require("../yearsAgo");
 
-describe('The isWeekday function...', function () {
-    it('should show the exact number of years ago a past date is', function () {
+describe('The yearsAgo function...', function () {
+    it('should calculate the exact number of years ago the year 1976 is', function () {
         assert.equal((new Date().getFullYear() - 1976), yearsAgo(1976))
+    });
+
+    it('should calculate the exact number of years ago the year 2000 is', function () {
         assert.equal((new Date().getFullYear() - 2000), yearsAgo(2000));
     });
 });
