b80a8cc23694fd0c6d14af31620da2069488a0b8
Pururu <pururumphahlele@gmail.com>
2021-09-07 14:51:06 +0200

    Add the transportFee file for testing.

---
+++ test/transportFee.test.js
@@ -0,0 +1,16 @@
+let assert = require("assert");
+let transportFee = require("../transportFee");
+
+
+describe('The transportFee function...', function () {
+    it('should return a R20 transport cost if I work a morning shift', function () {
+        assert.equal(transportFee('morning'), 'R20');
+    });
+    it('should return a R10 transport cost if I work an afternoon shift', function () {
+        assert.equal(transportFee('afternoon'), 'R10');
+        assert.equal(transportFee('nightshift'), 'free', 'for night shift return free');
+    });
+    it('should return no transport cost if I work nightshift', function () {
+        assert.equal(transportFee('nightshift'), 'free', 'for night shift return free');
+    });
+});
---
+++ transportFee.js
@@ -0,0 +1,11 @@
+module.export = function(shift){
+    if (shift == 'morning'){
+      return 'R20';
+    }
+    else if(shift == 'afternoon'){
+      return 'R10';
+    }
+    else {
+      return 'free'
+    }
+  };
