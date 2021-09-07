94e32de23bab11b14751e353e3609a820db0067d
Pururu <pururumphahlele@gmail.com>
2021-09-07 22:47:37 +0200

    Add scenarios to countAllFromPaarl.test file for testing.

--- test/countAllPaarl.test.js
+++ test/countAllPaarl.test.js
@@ -5,6 +5,9 @@ let countAllPaarl = require("../countAllPaarl");
 describe('The countAllPaarl function...', function () {
     it('should count the exact number of vehicle registration numbers from Paarl in a string', function () {
         assert.equal(3, countAllPaarl('CJ 345 123, CJ 2345, CL 123-546, CK 345, CJ 123'));
+    });
+
+    it('should count the exact number of vehicle registration numbers from Paarl in a string', function () {
         assert.equal(2, countAllPaarl('CJ 345 123, CK 345, CJ 123'));  
     });
 });
